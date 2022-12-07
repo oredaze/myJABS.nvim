35f13b5803009320d8b00fd4df99b86902c6f170
jeff <1105041+jeff-dh@users.noreply.github.com>
2022-12-07 01:53:14 +0100

    minor cleanup

--- plugin/jabs.lua
+++ plugin/jabs.lua
@@ -1,6 +1,6 @@
 local jabs = require('jabs')
 
-vim.api.nvim_create_user_command("JABS", jabs.open, {nargs = 0})
+vim.api.nvim_create_user_command("JABS", jabs.open, {})
 -- to stay backwards compatible
-vim.api.nvim_create_user_command("JABSOpen", jabs.open, {nargs = 0})
+vim.api.nvim_create_user_command("JABSOpen", jabs.open, {})
 
