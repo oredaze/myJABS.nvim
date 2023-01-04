6f8132844d51603f3e4b751480d51137e15dcb42
jeff <1105041+jeff-dh@users.noreply.github.com>
2023-01-04 15:58:29 +0100

    bugfix

--- lua/jabs/jabs.lua
+++ lua/jabs/jabs.lua
@@ -366,6 +366,9 @@ local function open()
         buf))
 
     api.nvim_open_win(buf, true, getPopupConfig())
+    -- hmm for some reason this line is neccessary to assign
+    -- the right highlighting to a minimal floating win....
+    vim.wo.winhl = 'Normal:Pmenu'
     vim.w.isJABSWindow = true
     vim.w.JABSCallerWinId = JABSCallerWinId
     vim.w.show_unlisted = false
